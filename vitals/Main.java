dadb84308ae422b3b4d47c5263f0559bee9ef5ca
saikumarchenna <121084246+saikumarchenna40@users.noreply.github.com>
2024-08-30 12:03:50 +0530

    Reduced complexity and used re-usable methods

--- vitals/Main.java
+++ vitals/Main.java
@@ -2,22 +2,44 @@ package vitals;
 
 public class Main {
     static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-        if(temperature < 0 || temperature > 45) {
-            System.out.println("Temperature is out of range!");
-            return false;
-        } else if(soc < 20 || soc > 80) {
-            System.out.println("State of Charge is out of range!");
-            return false;
-        } else if(chargeRate > 0.8) {
-            System.out.println("Charge Rate is out of range!");
-            return false;
-        }
-        return true;
+        return (validateTemperature(temperature) && validateSoc(soc) && validateChargeRate(chargeRate));
     }
 
+	static boolean validateTemperature(float temperature) {
+		boolean isValidTemp = false;
+		if (temperature >=0 && temperature <= 45) {
+			isValidTemp = true;
+		} else {
+			System.out.println("Temperature is out of range!");
+		}
+		return isValidTemp;
+		
+	}
+	
+	static boolean validateSoc(float soc) {
+		boolean isValidSoc = false;
+		if (soc >= 20 && soc <= 80) {
+			isValidSoc = true;
+		} else {
+			System.out.println("State of Charge is out of range!");
+		}
+		return isValidSoc;
+	}
+	
+	static boolean validateChargeRate(float chargeRate) {
+		boolean isValidChargeRate = false;
+		if (chargeRate <= 0.8) {
+			isValidChargeRate = true;
+		} else {
+			 System.out.println("Charge Rate is out of range!");
+		}
+		return isValidChargeRate;
+	}
+	
     public static void main(String[] args) {
-        assert(batteryIsOk(25, 70, 0.7f) == true);
-        assert(batteryIsOk(50, 85, 0.0f) == false);
-        System.out.println("Some more tests needed");
+        assert batteryIsOk(25, 70, 0.7f) == true;
+        assert batteryIsOk(20, 81, 0.0f) == false;
+        assert batteryIsOk(20, 60, 10.0f) == false;
+        assert batteryIsOk(46, 19, 0.8f) == false;
     }
 }
