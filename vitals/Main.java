5d0eed643ccb030ebdb7512a1b070f486a704c53
saikumarchenna <121084246+saikumarchenna40@users.noreply.github.com>
2024-10-16 16:03:16 +0530

    Update Main.java

--- vitals/Main.java
+++ vitals/Main.java
@@ -1,25 +1,40 @@
 package vitals;
 
 public class Main {
-   static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
+   static boolean batteryIsOk(float temperature, float soc, float chargeRate, int tolerencePercent) {
 	   System.out.println("Received Temperature: " + temperature);
 	   System.out.println("Received SoC: " + soc);
 	   System.out.println("Received ChargeRate: " + chargeRate);
-    return isWithinRange(temperature, 0, 45) && isWithinRange(soc, 20, 80) && isChargeRateValid(chargeRate);
+    return isWithinRange(temperature, 0, 45, true, tolerencePercent) && isWithinRange(soc, 20, 80, true, tolerencePercent) && isChargeRateValid(chargeRate, true, tolerencePercent);
 }
 
-static boolean isWithinRange(float value, float min, float max) {
+static boolean isWithinRange(float value, float min, float max, boolean isWarningRequired, int tolerencePercent) {
+	if (isWarningRequired) {
+		validateTolerance(value, min, max, tolerencePercent);
+	}
     return value >= min && value <= max;
 }
-
-static boolean isChargeRateValid(float chargeRate) {
+	
+static void validateTolerance(float value, float min, float max, int tolerencePercent) {
+	float tolerance = max * (tolerencePercent * 1f);
+	if(value >= min && value <= (min + tolerance)) {
+		 System.out.println("Warning: Approaching discharge");
+	} else if (value >= (max - tolerance) && value <= max) {
+		System.out.println("Warning: Approaching charge-peak");
+	}
+}
+	
+static boolean isChargeRateValid(float chargeRate, boolean isWarningRequired, int tolerencePercent) {
+	if (isWarningRequired) {
+		validateTolerance(chargeRate, Float.MIN_VALUE, 0.8f, tolerencePercent);
+	}
     return chargeRate <= 0.8;
 }
 	
     public static void main(String[] args) {
-        assert batteryIsOk(25, 70, 0.7f) == true;
-        assert batteryIsOk(20, 81, 0.0f) == false;
-        assert batteryIsOk(20, 60, 10.0f) == false;
-        assert batteryIsOk(46, 19, 0.8f) == false;
+        assert batteryIsOk(25, 70, 0.7f, 5) == true;
+        assert batteryIsOk(20, 81, 0.0f, 5) == false;
+        assert batteryIsOk(20, 60, 10.0f, 5) == false;
+        assert batteryIsOk(46, 19, 0.8f, 5) == false;
     }
 }
